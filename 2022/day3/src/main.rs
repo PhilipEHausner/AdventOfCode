52c23d9876748d288b4aa3006f82ddce2bf3a314
Philip Hausner <Philip.E.Hausner@gmail.com>
2022-12-03 13:41:16 +0100

    2022 - day 3 part 2

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -1,27 +1,57 @@
 mod item_priorities;
 
+use std::collections::HashSet;
+
 use item_priorities::get_item_priority;
 use util::read_files::read_file_as_vector;
 
 fn main() {
     let result1 = solve1("./files/day3.txt");
     println!("Solution problem 1: {}", result1);
+    let result2 = solve2("./files/day3.txt");
+    println!("Solution problem 2: {}", result2);
 }
 
 fn solve1(filename: &str) -> u64 {
     let backpacks = read_file_as_vector(filename).expect("Error reading file.");
 
-    backpacks.into_iter()
+    backpacks
+        .into_iter()
         .map(|backpack| priority_of_double_item(&backpack))
         .sum()
 }
 
-fn priority_of_double_item(backpack: &String) -> u64 {
+fn priority_of_double_item(backpack: &str) -> u64 {
     let compartment1 = &backpack[..backpack.len() / 2];
     let compartment2 = &backpack[backpack.len() / 2..];
 
-    for c in compartment1.chars() {
-        if compartment2.contains(c) { return get_item_priority(c); }
+    for item in compartment1.chars() {
+        if compartment2.contains(item) {
+            return get_item_priority(item);
+        }
+    }
+
+    0
+}
+
+fn solve2(filename: &str) -> u64 {
+    let backpacks = read_file_as_vector(filename).expect("Error reading file.");
+    debug_assert_eq!(backpacks.len() % 3, 0);
+
+    let mut result = 0;
+    for i in (0..backpacks.len()).step_by(3) {
+        result += priority_of_group_badge(&backpacks[i], &backpacks[i+1],&backpacks[i+2]);
+    }
+    
+    result
+}
+
+fn priority_of_group_badge(backpack1: &str, backpack2: &str, backpack3: &str) -> u64 {
+    let items2: HashSet<char> = HashSet::from_iter(backpack2.chars());
+    let items3: HashSet<char> = HashSet::from_iter(backpack3.chars());
+
+    for item in backpack1.chars() {
+        if items2.contains(&item) &&  items3.contains(&item) { return get_item_priority(item); }
     }
 
     0
@@ -38,10 +68,30 @@ mod tests {
 
     #[test]
     fn test_priority_of_double_item() {
-        assert_eq!(priority_of_double_item(&"abcgbf".to_string()), 2);
-        assert_eq!(priority_of_double_item(&"aXpoqcgbXf".to_string()), 50);
-        assert_eq!(priority_of_double_item(&"uhgfylkpoy".to_string()), 25);
-        assert_eq!(priority_of_double_item(&"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".to_string()), 38);
+        assert_eq!(priority_of_double_item("abcgbf"), 2);
+        assert_eq!(priority_of_double_item("aXpoqcgbXf"), 50);
+        assert_eq!(priority_of_double_item("uhgfylkpoy"), 25);
+        assert_eq!(
+            priority_of_double_item("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"),
+            38
+        );
+    }
+
+    #[test]
+    fn test_solve2() {
+        assert_eq!(solve2("./files/test.txt"), 70);
+    }
+
+    #[test]
+    fn test_priority_of_group_badge() {
+        assert_eq!(
+            priority_of_group_badge(
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg"
+            ),
+            18
+        );
+        assert_eq!(priority_of_group_badge("abcdef", "poejkfqwe", "mnbwe"), 5);
     }
 }
-
