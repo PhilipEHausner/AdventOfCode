3d1e428c3cfd64cd49ef314e2b0682b6dadb9107
Philip Hausner <Philip.E.Hausner@gmail.com>
2022-12-04 13:23:34 +0100

    2022 - Day4 part 1

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -1,11 +1,64 @@
+use util::read_files::read_file_as_vector;
+
+struct AssignmentRange {
+    pub start: u64,
+    pub end: u64,
+}
+
+struct ElvePair {
+    assignment_elve1: AssignmentRange,
+    assignment_elve2: AssignmentRange,
+}
+
+impl ElvePair {
+    pub fn do_fully_overlap(&self) -> bool {
+        let (start1, end1) = (self.assignment_elve1.start, self.assignment_elve1.end);
+        let (start2, end2) = (self.assignment_elve2.start, self.assignment_elve2.end);
+
+        if (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1) {
+            true
+        } else {
+            false
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let solution1 = solve1("./files/day4.txt");
+    println!("Solution problem1: {}", solution1);
 }
 
 fn solve1(filename: &str) -> u64 {
-    0
+    let lines = read_file_as_vector(filename).expect("Error reading file.");
+    let elve_pairs = get_elve_pairs(&lines);
+    elve_pairs
+        .iter()
+        .map(|pair| if pair.do_fully_overlap() { 1 } else { 0 })
+        .sum()
 }
 
+fn get_elve_pairs(lines: &Vec<String>) -> Vec<ElvePair> {
+    let ranges: Vec<Vec<&str>> = lines.iter().map(|line| line.split(",").collect()).collect();
+    ranges
+        .iter()
+        .map(|split| ElvePair {
+            assignment_elve1: parse_assignment_range(split[0]),
+            assignment_elve2: parse_assignment_range(split[1]),
+        })
+        .collect()
+}
+
+fn parse_assignment_range(range: &str) -> AssignmentRange {
+    let bounds: Vec<u64> = range
+        .split("-")
+        .map(|r| r.parse().expect("Bound of range invalid."))
+        .collect();
+    debug_assert_eq!(bounds.len(), 2);
+    AssignmentRange {
+        start: bounds[0],
+        end: bounds[1],
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -16,4 +69,45 @@ mod tests {
         assert_eq!(solve1("./files/test.txt"), 2);
     }
 
+    #[test]
+    fn test_elve_pairs_do_fully_overlap() {
+        test_elve_pairs_do_fully_overlap_helper(1, 4, 2, 3, true);
+        test_elve_pairs_do_fully_overlap_helper(2, 3, 1, 4, true);
+        test_elve_pairs_do_fully_overlap_helper(1, 4, 2, 5, false);
+        test_elve_pairs_do_fully_overlap_helper(2, 5, 1, 4, false);
+        test_elve_pairs_do_fully_overlap_helper(4, 4, 2, 5, true);
+        test_elve_pairs_do_fully_overlap_helper(2, 4, 1, 3, false);
+        test_elve_pairs_do_fully_overlap_helper(1, 3, 4, 5, false);
+        test_elve_pairs_do_fully_overlap_helper(4, 5, 1, 3, false);
+        test_elve_pairs_do_fully_overlap_helper(4, 4, 2, 5, true);
+    }
+
+    fn test_elve_pairs_do_fully_overlap_helper(
+        start1: u64,
+        end1: u64,
+        start2: u64,
+        end2: u64,
+        outcome: bool,
+    ) {
+        let pair = ElvePair {
+            assignment_elve1: AssignmentRange {
+                start: start1,
+                end: end1,
+            },
+            assignment_elve2: AssignmentRange {
+                start: start2,
+                end: end2,
+            },
+        };
+        assert_eq!(
+            pair.do_fully_overlap(),
+            outcome,
+            "Ranges are: {} - {} and {} - {}, expected outcome: {}",
+            start1,
+            end1,
+            start2,
+            end2,
+            outcome
+        );
+    }
 }
