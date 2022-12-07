e116ec12ffc794f7f102dbc7fe711b6a9e3c3d49
Philip Hausner <Philip.E.Hausner@gmail.com>
2022-12-07 23:33:21 +0100

    2022 - day 7 part 2

--- 2022/day7/src/file_tree/mod.rs
+++ 2022/day7/src/file_tree/mod.rs
@@ -6,7 +6,7 @@ pub enum NodeType {
 
 #[derive(Debug, Clone)]
 pub struct Node {
-    pub node_type: NodeType, 
+    pub node_type: NodeType,
     pub name: String,
     pub size: u64,
     pub children: Vec<Node>,
@@ -20,7 +20,10 @@ pub struct FileTree {
 impl Node {
     pub fn new(node_type: NodeType, name: &str, size: u64) -> Node {
         Node {
-            node_type, name: name.to_string(), size, children: vec![],
+            node_type,
+            name: name.to_string(),
+            size,
+            children: vec![],
         }
     }
 
@@ -29,8 +32,8 @@ impl Node {
             if child.name == child_name {
                 return child;
             }
-        };
-        panic!("No child with name {}", child_name);        
+        }
+        panic!("No child with name {}", child_name);
     }
 
     #[allow(dead_code)]
@@ -47,7 +50,9 @@ impl Node {
 
     #[allow(dead_code)]
     fn print_dir_sizes(&self) {
-        if self.node_type == NodeType::Folder { println!("{} ({})", self.name, self.size); };
+        if self.node_type == NodeType::Folder {
+            println!("{} ({})", self.name, self.size);
+        };
         for child in &self.children {
             child.print_dir_sizes();
         }
@@ -55,7 +60,7 @@ impl Node {
 
     fn compute_sub_sizes(&mut self) -> u64 {
         let mut size = self.size;
-        
+
         for child in &mut self.children {
             let child_size = child.compute_sub_sizes();
             size += child_size;
@@ -66,15 +71,31 @@ impl Node {
     }
 
     fn get_sub_100k_dir_sizes_sum(&self) -> u64 {
-        if self.node_type == NodeType::File { return 0; }
+        if self.node_type == NodeType::File {
+            return 0;
+        }
 
-        let mut size = if self.size < 100000 {self.size} else {0};
-        
+        let mut size = if self.size < 100000 { self.size } else { 0 };
+
         for child in &self.children {
             size += child.get_sub_100k_dir_sizes_sum();
         }
 
         size
+    }
+
+    fn get_all_dir_sizes(&self) -> Vec<u64> {
+        if self.node_type == NodeType::File {
+            return vec![];
+        }
+
+        let mut sizes = vec![self.size];
+
+        for child in &self.children {
+            sizes.append(&mut child.get_all_dir_sizes());
+        }
+
+        sizes
     }
 }
 
@@ -93,7 +114,7 @@ impl FileTree {
     #[allow(dead_code)]
     pub fn print(&self) {
         self.root.print();
-    }    
+    }
 
     #[allow(dead_code)]
     pub fn print_dir_sizes(&self) {
@@ -106,5 +127,17 @@ impl FileTree {
 
     pub fn get_sub_100k_dir_sizes_sum(&self) -> u64 {
         self.root.get_sub_100k_dir_sizes_sum()
+    }
+
+    pub fn smallest_to_have_less_than_400k_mem(&self) -> u64 {
+        let total_size = self.root.size;
+        let target = total_size - 40_000_000;
+        let mut dir_sizes = self.root.get_all_dir_sizes();
+        dir_sizes = dir_sizes
+            .iter()
+            .filter(|s| **s >= target)
+            .map(|s| *s)
+            .collect::<Vec<u64>>();
+        return *dir_sizes.iter().min().unwrap();
     }
 }
--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -12,22 +12,30 @@ enum Command {
 
 fn main() {
     let lines = read_file_as_vector("./files/day7.txt").expect("Error reading file.");
-    println!("Solution part 1 {}", solve1(lines));
+    println!("Solution part 1 {}", solve1(&lines));
+    println!("Solution part 2 {}", solve2(&lines));
 }
 
-fn solve1(lines: Vec<String>) -> u64 {
+fn solve1(lines: &Vec<String>) -> u64 {
     let tree = create_file_tree(&lines);
     tree.get_sub_100k_dir_sizes_sum()
+}
+
+fn solve2(lines: &Vec<String>) -> u64 {
+    let tree = create_file_tree(&lines);
+    tree.smallest_to_have_less_than_400k_mem()
 }
 
 fn create_file_tree(lines: &Vec<String>) -> FileTree {
     let root_node = Node::new(NodeType::Folder, "ROOT", 0);
     let mut current_path: Vec<String> = vec![];
-    let mut tree = FileTree{root: root_node};
+    let mut tree = FileTree { root: root_node };
 
     let mut curr_index = 0;
     loop {
-        if curr_index >= lines.len() { break; }
+        if curr_index >= lines.len() {
+            break;
+        }
         let line = &lines[curr_index];
         let next_command = get_next_command(&line);
 
@@ -35,7 +43,7 @@ fn create_file_tree(lines: &Vec<String>) -> FileTree {
             match &line[5..] {
                 "/" => {
                     current_path = vec![];
-                },
+                }
                 ".." => {
                     current_path.pop();
                 }
@@ -47,16 +55,28 @@ fn create_file_tree(lines: &Vec<String>) -> FileTree {
         } else if next_command == Command::List {
             loop {
                 curr_index += 1;
-                if curr_index >= lines.len() { break; }
+                if curr_index >= lines.len() {
+                    break;
+                }
                 let list_line = &lines[curr_index];
                 if &list_line[..3] == "dir" {
-                    let new_child = Node { node_type: NodeType::Folder, name: list_line[4..].to_string(), size: 0, children: vec![] };
+                    let new_child = Node {
+                        node_type: NodeType::Folder,
+                        name: list_line[4..].to_string(),
+                        size: 0,
+                        children: vec![],
+                    };
                     tree.append_child(&current_path, new_child);
-                } else if &list_line[0..1] == "$" { 
+                } else if &list_line[0..1] == "$" {
                     break;
                 } else {
                     let file_line = list_line.split(" ").collect::<Vec<&str>>();
-                    let new_child = Node { node_type: NodeType::File, name: file_line[1].to_string(), size: file_line[0].parse::<u64>().unwrap(), children: vec![] };
+                    let new_child = Node {
+                        node_type: NodeType::File,
+                        name: file_line[1].to_string(),
+                        size: file_line[0].parse::<u64>().unwrap(),
+                        children: vec![],
+                    };
                     tree.append_child(&current_path, new_child);
                 }
             }
@@ -71,13 +91,11 @@ fn create_file_tree(lines: &Vec<String>) -> FileTree {
 
 fn get_next_command(line: &str) -> Command {
     match &line[..4] {
-        "$ cd" => { Command::Change },
-        "$ ls" => { Command::List },
-        _ => { Command::ListEntry },
+        "$ cd" => Command::Change,
+        "$ ls" => Command::List,
+        _ => Command::ListEntry,
     }
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -86,6 +104,12 @@ mod tests {
     #[test]
     fn test_solve1() {
         let lines = read_file_as_vector("./files/test.txt").expect("Error reading file.");
-        assert_eq!(solve1(lines), 95437);
+        assert_eq!(solve1(&lines), 95437);
+    }
+
+    #[test]
+    fn test_solve2() {
+        let lines = read_file_as_vector("./files/test.txt").expect("Error reading file.");
+        assert_eq!(solve2(&lines), 24933642);
     }
 }
