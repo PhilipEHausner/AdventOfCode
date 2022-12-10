6a4e1d94178dc2af376202f8f6dd03bec4dbddb2
Philip Hausner <Philip.E.Hausner@gmail.com>
2022-12-10 12:51:42 +0100

    2022 - Day 10 part 2

--- 2022/day10/src/main.rs
+++ 2022/day10/src/main.rs
@@ -14,6 +14,8 @@ impl Command {
 fn main() {
     let lines = read_file_as_vector("./files/day10.txt").expect("Error reading file.");
     println!("Solution part 1: {}", solve1(&lines));
+    println!("Solution part 2:");
+    solve2(&lines);
 }
 
 fn solve1(instructions: &Vec<String>) -> i64 {
@@ -37,6 +39,26 @@ fn solve1(instructions: &Vec<String>) -> i64 {
     result
 }
 
+fn solve2(instructions: &Vec<String>) {
+    let mut cycle: usize = 0;
+    let mut sprite_position: i64 = 1;
+    let mut commands = vec![];
+    let mut screen = Vec::<&str>::new();
+
+    for instruction in instructions {
+        commands.append(&mut process_instruction(instruction));
+    }
+
+    for command in commands {
+        let does_overlap = pixel_overlaps_with_sprite(cycle, sprite_position);
+        screen.push(if does_overlap { "#" } else { "." });
+        sprite_position += command.add;
+        cycle += 1;
+    }
+
+    print_screen(&screen);
+}
+
 fn process_instruction(instruction: &String) -> Vec<Command> {
     let command = &instruction[..4];
 
@@ -51,6 +73,19 @@ fn process_instruction(instruction: &String) -> Vec<Command> {
         _ => panic!("Invalid command."),
     }
 }
+
+fn pixel_overlaps_with_sprite(pixel_pos: usize, sprite_pos: i64) -> bool {
+    sprite_pos < 0 || ( pixel_pos % 40 >= sprite_pos as usize - 1 && pixel_pos % 40 <= sprite_pos as usize + 1 )
+}
+
+fn print_screen(screen: &Vec<&str>) {
+    for i in 0..6 {
+        let start_pos = 40 * i;
+        let end_pos = 40 * i + 40;
+        println!("{}", screen[start_pos..end_pos].join(""));
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
