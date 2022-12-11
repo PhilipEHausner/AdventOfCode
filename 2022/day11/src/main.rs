6797af0174bf62542d87c2bd96de11a566a02d2d
Philip Hausner <Philip.E.Hausner@gmail.com>
2022-12-11 14:41:18 +0100

    2022 - Day 11 Part 1

--- 2022/day11/src/main.rs
+++ 2022/day11/src/main.rs
@@ -1,14 +1,56 @@
+mod monkey;
+
+use monkey::Monkey;
 use util::read_files::read_file_as_vector;
 
 fn main() {
-    let lines = read_file_as_vector("./files/day10.txt").expect("Error reading file.");
-    println!("Solution part 1: {}", solve1(&lines));
+    let lines = read_file_as_vector("./files/day11.txt").expect("Error reading file.");
+    println!("Solution part 1: {}", solve1(&lines, 20));
 }
 
-fn solve1(lines: &Vec<String>) -> u64 {
-    0
-} 
+fn solve1(lines: &Vec<String>, rounds: usize) -> u64 {
+    let mut monkeys = parse_monkeys(lines, 3);
 
+    for _ in 0..rounds {
+        monkey_round(&mut monkeys);
+    }
+    
+    let mut inspections = monkeys.iter().map(|monkey| monkey.num_inspected_items()).collect::<Vec<u64>>();
+    inspections.sort_by_key(|&w| std::cmp::Reverse(w));
+    inspections[0] * inspections[1]
+}
+
+fn parse_monkeys(lines: &Vec<String>, worry_decay: u64) -> Vec<Monkey> {
+    let mut monkeys = vec![];
+    for i in (0..lines.len()).step_by(7) {
+        let number = &lines[i + 0];
+        let items = &lines[i + 1];
+        let operation = &lines[i + 2];
+        let decision_test = &lines[i + 3];
+        let decision_true = &lines[i + 4];
+        let decision_false = &lines[i + 5];
+        let monkey = Monkey::new_from_strings(
+            number,
+            items,
+            operation,
+            decision_test,
+            decision_true,
+            decision_false,
+            worry_decay,
+        );
+        monkeys.push(monkey);
+    }
+    monkeys
+}
+
+fn monkey_round(monkeys: &mut Vec<Monkey>) {
+    for i in 0..monkeys.len() {
+        while monkeys[i].has_next_item() {
+            let monkey_throw = monkeys[i].process_next_item();
+            monkeys[monkey_throw.target_monkey].push_item(monkey_throw.item_worry_level);
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -17,6 +59,6 @@ mod tests {
     #[test]
     fn test_solve1() {
         let lines = read_file_as_vector("./files/test.txt").expect("Error reading file.");
-        assert_eq!(solve1(&lines), 10605);
+        assert_eq!(solve1(&lines, 20), 10605);
     }
 }
---
+++ 2022/day11/src/monkey/mod.rs
@@ -0,0 +1,155 @@
+#[derive(Debug)]
+enum Operation {
+    Add(u64),
+    Mult(u64),
+    Square,
+}
+
+#[derive(Debug)]
+pub struct MonkeyThrow {
+    pub target_monkey: usize,
+    pub item_worry_level: u64,
+}
+
+#[derive(Debug)]
+pub struct Monkey {
+    _number: u64,
+    pub items: Vec<u64>,
+    operation: Operation,
+    decision_test: u64,
+    decision_true: usize,
+    decision_false: usize,
+    worry_decay: u64,
+    inspected_items: u64,
+}
+
+impl Monkey {
+    pub fn has_next_item(&self) -> bool {
+        self.items.len() > 0
+    }
+
+    pub fn process_next_item(&mut self) -> MonkeyThrow {
+        self.inspected_items += 1;
+
+        let next_item = self.items.pop().unwrap();
+        let item_worry_level = self.updated_item_worry_level(next_item);
+        let target_monkey = if item_worry_level % self.decision_test == 0 {
+            self.decision_true
+        } else {
+            self.decision_false
+        };
+
+        MonkeyThrow {
+            target_monkey,
+            item_worry_level,
+        }
+    }
+
+    fn updated_item_worry_level(&self, item_worry_level: u64) -> u64 {
+        let mut worry_level = item_worry_level;
+        match self.operation {
+            Operation::Add(x) => {
+                worry_level += x;
+            }
+            Operation::Mult(x) => {
+                worry_level *= x;
+            }
+            Operation::Square => {
+                worry_level *= worry_level;
+            }
+        }
+        worry_level /= self.worry_decay;
+        worry_level
+    }
+
+    pub fn push_item(&mut self, item_worry_level: u64) {
+        self.items.push(item_worry_level);
+    }
+
+    pub fn num_inspected_items(&self) -> u64 {
+        self.inspected_items
+    }
+}
+
+// PARSER
+impl Monkey {
+    pub fn new_from_strings(
+        number: &String,
+        items: &String,
+        operation: &String,
+        decision_test: &String,
+        decision_true: &String,
+        decision_false: &String,
+        worry_decay: u64,
+    ) -> Monkey {
+        let m_number = Monkey::parse_number(number);
+        let m_items = Monkey::parse_items(items);
+        let m_operation = Monkey::parse_operation(operation);
+        let m_decision_test = Monkey::parse_decision_test(decision_test);
+        let m_decision_true = Monkey::parse_decision_true(decision_true);
+        let m_decision_false = Monkey::parse_decision_false(decision_false);
+
+        Monkey {
+            _number: m_number,
+            items: m_items,
+            operation: m_operation,
+            decision_test: m_decision_test,
+            decision_true: m_decision_true,
+            decision_false: m_decision_false,
+            inspected_items: 0,
+            worry_decay,
+        }
+    }
+
+    fn parse_number(number: &String) -> u64 {
+        number.split(" ").collect::<Vec<&str>>()[1]
+            .replace(":", "")
+            .parse::<u64>()
+            .expect("Error parsing number.")
+    }
+
+    fn parse_items(items: &String) -> Vec<u64> {
+        items
+            .trim()
+            .replace(",", "")
+            .split(" ")
+            .collect::<Vec<&str>>()
+            .iter()
+            .skip(2)
+            .map(|el| el.parse::<u64>().expect("Error parsing item number."))
+            .collect::<Vec<u64>>()
+    }
+
+    fn parse_operation(operation: &String) -> Operation {
+        let op = operation.trim().split(" ").skip(4).collect::<Vec<&str>>();
+
+        match op[..2] {
+            ["*", "old"] => Operation::Square,
+            ["*", _] => Operation::Mult(op[1].parse().unwrap()),
+            ["+", _] => Operation::Add(op[1].parse().unwrap()),
+            _ => panic!("Invalid operation: {:?}", op),
+        }
+    }
+
+    fn parse_decision_test(decision_test: &String) -> u64 {
+        Monkey::parse_decision_generic(decision_test) as u64
+    }
+
+    fn parse_decision_true(decision_true: &String) -> usize {
+        Monkey::parse_decision_generic(decision_true)
+    }
+
+    fn parse_decision_false(decision_false: &String) -> usize {
+        Monkey::parse_decision_generic(decision_false)
+    }
+
+    fn parse_decision_generic(line: &String) -> usize {
+        line.trim()
+            .split(" ")
+            .collect::<Vec<&str>>()
+            .last()
+            .unwrap()
+            .parse()
+            .unwrap()
+    }
+}
