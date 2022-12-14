a5dfc0022ef51b445dff98a8526bf52bba009176
Philip Hausner <Philip.E.Hausner@gmail.com>
2022-12-14 20:11:01 +0100

    2022 - Day 14 formatting

--- 2022/day14/src/main.rs
+++ 2022/day14/src/main.rs
@@ -2,7 +2,6 @@ use std::{cmp, vec};
 
 use util::read_files::read_file_as_vector;
 
-
 fn main() {
     let lines = read_file_as_vector("./files/day14.txt").expect("Error reading file.");
     println!("Solution part 1: {}", solve1(&lines));
@@ -33,24 +32,39 @@ fn create_cave(lines: &Vec<String>) -> Vec<Vec<char>> {
     let mut cave = vec![vec!['.'; 1000]; 1000];
 
     for line in lines {
-        let points = line.split("->").map(|el| {
-            (
-                el.trim().split(",").nth(1).unwrap().parse::<usize>().unwrap(),
-                el.trim().split(",").nth(0).unwrap().parse::<usize>().unwrap(),
-            )
-        }).collect::<Vec<(usize, usize)>>();
+        let points = line
+            .split("->")
+            .map(|el| {
+                (
+                    el.trim()
+                        .split(",")
+                        .nth(1)
+                        .unwrap()
+                        .parse::<usize>()
+                        .unwrap(),
+                    el.trim()
+                        .split(",")
+                        .nth(0)
+                        .unwrap()
+                        .parse::<usize>()
+                        .unwrap(),
+                )
+            })
+            .collect::<Vec<(usize, usize)>>();
 
         for i in 1..points.len() {
-            let start_point = points[i-1];
+            let start_point = points[i - 1];
             let end_point = points[i];
             cave[start_point.0][start_point.1] = '#';
             cave[end_point.0][end_point.1] = '#';
             if start_point.0 == end_point.0 {
-                for j in cmp::min(start_point.1, end_point.1)..cmp::max(start_point.1, end_point.1) {
+                for j in cmp::min(start_point.1, end_point.1)..cmp::max(start_point.1, end_point.1)
+                {
                     cave[start_point.0][j] = '#';
                 }
             } else if start_point.1 == end_point.1 {
-                for j in cmp::min(start_point.0, end_point.0)..cmp::max(start_point.0, end_point.0) {
+                for j in cmp::min(start_point.0, end_point.0)..cmp::max(start_point.0, end_point.0)
+                {
                     cave[j][start_point.1] = '#';
                 }
             }
@@ -91,7 +105,13 @@ fn throw_sand(cave: &mut Vec<Vec<char>>) -> bool {
 }
 
 fn create_floor(cave: &mut Vec<Vec<char>>) {
-    let floor_level = cave.iter().enumerate().map(|(i,row)| if row.contains(&'#') { i } else { 0 }).max().unwrap() + 2;
+    let floor_level = cave
+        .iter()
+        .enumerate()
+        .map(|(i, row)| if row.contains(&'#') { i } else { 0 })
+        .max()
+        .unwrap()
+        + 2;
 
     cave[floor_level] = vec!['#'; 1000];
 }
