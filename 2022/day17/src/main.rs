a548863668a7a302cce3de7e12cca926788f002c
Philip Hausner <Philip.E.Hausner@gmail.com>
2022-12-17 11:53:13 +0100

    2022 - Day 17 Preparation

---
+++ 2022/day17/src/main.rs
@@ -0,0 +1,22 @@
+use util::read_files::read_file_as_vector;
+
+fn main() {
+    let lines = read_file_as_vector("./files/day17.txt").expect("Error reading file.");
+    println!("Solution part 1: {}", solve1(&lines));
+}
+
+fn solve1(lines: &Vec<String>) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve1() {
+        let lines = read_file_as_vector("./files/test.txt").expect("Error reading file.");
+        assert_eq!(solve1(&lines), 3068);
+    }
+
+}
