0c6f50d8b4ee51854429fcb2916e27d66bbe1e6a
PhilipEHausner <Philip.E.Hausner@gmail.com>
2023-06-08 16:15:22 +0200

    2019 - day 1 part 2

--- 2019/day1/src/main/kotlin/Main.kt
+++ 2019/day1/src/main/kotlin/Main.kt
@@ -8,13 +8,30 @@ fun computeFuel(mass: Int): Int {
     return (mass / 3) - 2
 }
 
+fun computeTotalFuel(mass: Int): Int {
+    var totalFuel = computeFuel(mass)
+    var newFuel = computeFuel(totalFuel)
+    while (newFuel > 0) {
+        totalFuel += newFuel
+        newFuel = computeFuel(newFuel)
+    }
+    return totalFuel
+}
+
 fun part1(numbers: List<Int>) {
     var fuel = 0;
     numbers.forEach { fuel += computeFuel(it) }
     println("Solution for part 1: $fuel")
 }
 
+fun part2(numbers: List<Int>) {
+    var fuel = 0
+    numbers.forEach { fuel += computeTotalFuel(it) }
+    println("Solution for part 2: $fuel")
+}
+
 fun main() {
     val numbers = readFile("files/day1.txt")
     part1(numbers)
+    part2(numbers)
 }
--- 2019/day1/src/test/kotlin/MainKtTest.kt
+++ 2019/day1/src/test/kotlin/MainKtTest.kt
@@ -5,9 +5,16 @@ internal class MainKtTest {
 
     @Test
     fun testComputeFuel() {
-        assertEquals(computeFuel(12), 2)
-        assertEquals(computeFuel(14), 2)
-        assertEquals(computeFuel(1969), 654)
-        assertEquals(computeFuel(100756), 33583)
+        assertEquals(2, computeFuel(12))
+        assertEquals(2, computeFuel(14))
+        assertEquals(654, computeFuel(1969))
+        assertEquals(33583, computeFuel(100756))
+    }
+
+    @Test
+    fun testComputeTotalFuel() {
+        assertEquals(2, computeTotalFuel(14))
+        assertEquals(966, computeTotalFuel(1969))
+        assertEquals(50346, computeTotalFuel(100756))
     }
 }
