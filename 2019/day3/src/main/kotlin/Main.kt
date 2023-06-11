769e3b45f943f4ad2f3b15462e715432c96577d7
PhilipEHausner <Philip.E.Hausner@gmail.com>
2023-06-11 15:36:21 +0200

    2019 - Day 3 Part 2

--- 2019/day3/src/main/kotlin/Main.kt
+++ 2019/day3/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import java.io.File
 import java.security.InvalidParameterException
+import javax.management.openmbean.InvalidKeyException
 import kotlin.math.absoluteValue
 
 enum class Direction(val direction: Int) {
@@ -45,44 +46,62 @@ fun readFile(file: String): Pair<List<Movement>, List<Movement>> {
     return Pair(mLists[0], mLists[1])
 }
 
-fun getVisitedPoints(path: List<Movement>): Set<Pair<Int, Int>> {
+fun getVisitedPoints(path: List<Movement>): Pair<Set<Pair<Int, Int>>, Map<Pair<Int, Int>, Int>> {
     var currentPoint = Pair(0, 0)
     val visitedPoints = mutableSetOf<Pair<Int, Int>>()
+    val pointToSteps = mutableMapOf<Pair<Int, Int>, Int>()
+    var steps = 0
 
     for (movement in path) {
         val direction = movement.getDirectionVector()
         for (i in 0 until movement.range) {
             currentPoint = Pair(currentPoint.first + direction.first, currentPoint.second + direction.second)
+            steps += 1
+            if (currentPoint !in pointToSteps.keys) {
+                pointToSteps[currentPoint] = steps
+            }
             visitedPoints.add(currentPoint)
         }
     }
 
-    return visitedPoints
+    return Pair(visitedPoints, pointToSteps)
 }
 
-fun getIntersections(path1: List<Movement>, path2: List<Movement>): List<Pair<Int, Int>> {
-    val intersections = mutableListOf<Pair<Int, Int>>()
+fun getIntersections(path1: List<Movement>, path2: List<Movement>): List<Triple<Int, Int, Int>> {
+    val intersections = mutableListOf<Triple<Int, Int, Int>>()
 
     val path1Points = getVisitedPoints(path1)
     val path2Points = getVisitedPoints(path2)
 
-    for (point in path1Points) {
-        if (point != Pair(0, 0) && point in path2Points) {
-            intersections.add(point)
+    for (point in path1Points.first) {
+        if (point != Pair(0, 0) && point in path2Points.first) {
+            intersections.add(
+                Triple(
+                    point.first,
+                    point.second,
+                    (path1Points.second[point] ?: throw InvalidKeyException()) + (path2Points.second[point]
+                        ?: throw InvalidKeyException())
+                )
+            )
         }
     }
 
     return intersections
 }
 
-fun getClosestDistance(intersections: List<Pair<Int, Int>>): Int {
+fun getClosestManhattanDistance(intersections: List<Triple<Int, Int, Int>>): Int {
     return intersections.minOfOrNull { it.first.absoluteValue + it.second.absoluteValue }
         ?: throw InvalidParameterException("Empty list")
 }
 
 fun getMinimalManhattanDistanceOfIntersections(mLists: Pair<List<Movement>, List<Movement>>): Int {
     val intersections = getIntersections(mLists.first, mLists.second)
-    return getClosestDistance(intersections)
+    return getClosestManhattanDistance(intersections)
+}
+fun getMinimalSignalDelay(mLists: Pair<List<Movement>, List<Movement>>): Int {
+    val intersections = getIntersections(mLists.first, mLists.second)
+    return intersections.minOfOrNull { it.third }
+        ?: throw InvalidParameterException("Empty list")
 }
 
 fun part1(mLists: Pair<List<Movement>, List<Movement>>) {
@@ -90,7 +109,13 @@ fun part1(mLists: Pair<List<Movement>, List<Movement>>) {
     println("The Manhattan distance to the closest intersection is ${minDistance}.")
 }
 
+fun part2(mLists: Pair<List<Movement>, List<Movement>>) {
+    val minDistance = getMinimalSignalDelay(mLists)
+    println("The signal delay to the closest intersection is ${minDistance}.")
+}
+
 fun main() {
     val mLists = readFile("files/day3.txt")
     part1(mLists)
+    part2(mLists)
 }
--- 2019/day3/src/test/kotlin/MainKtTest.kt
+++ 2019/day3/src/test/kotlin/MainKtTest.kt
@@ -8,7 +8,7 @@ internal class MainKtTest {
     }
 
     @Test
-    fun testComputeProgramAlarm() {
+    fun testGetMinimalManhattanDistanceOfIntersections() {
         assertEquals(
             6,
             getMinimalManhattanDistanceOfIntersections(
@@ -30,6 +30,37 @@ internal class MainKtTest {
         assertEquals(
             135,
             getMinimalManhattanDistanceOfIntersections(
+                Pair(
+                    stringToMovements("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+                    stringToMovements("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+                )
+            )
+        )
+    }
+
+    @Test
+    fun testGetMinimalSignalDelay() {
+        assertEquals(
+            30,
+            getMinimalSignalDelay(
+                Pair(
+                    stringToMovements("R8,U5,L5,D3"),
+                    stringToMovements("U7,R6,D4,L4")
+                )
+            )
+        )
+        assertEquals(
+            610,
+            getMinimalSignalDelay(
+                Pair(
+                    stringToMovements("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+                    stringToMovements("U62,R66,U55,R34,D71,R55,D58,R83")
+                )
+            )
+        )
+        assertEquals(
+            410,
+            getMinimalSignalDelay(
                 Pair(
                     stringToMovements("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
                     stringToMovements("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
