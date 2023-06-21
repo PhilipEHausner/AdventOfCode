dbe0c63d7de946682f9c376ae7f3b018667668ca
PhilipEHausner <Philip.E.Hausner@gmail.com>
2023-06-21 11:49:27 +0200

    2019 - Day 9 Add tests

--- 2019/day9/src/test/kotlin/IntcodeComputerTest.kt
+++ 2019/day9/src/test/kotlin/IntcodeComputerTest.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -15,22 +16,34 @@ class IntcodeComputerTest {
     @Test
     fun testProcess() {
         val tests = listOf(
-            TestInput(
-                this.stringToLongList("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"), 109
-            ),
-            TestInput(
-                this.stringToLongList("1102,34915192,34915192,7,4,7,99,0"), 1219070632396864
-            ),
-            TestInput(
-                this.stringToLongList("104,1125899906842624,99"), 1125899906842624
-            ),
-            TestInput(
-                this.stringToLongList("109,-1,4,1,99"), -1
-            )
+            TestInput(this.stringToLongList("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"), 109),
+            TestInput(this.stringToLongList("1102,34915192,34915192,7,4,7,99,0"), 1219070632396864),
+            TestInput(this.stringToLongList("104,1125899906842624,99"), 1125899906842624),
+            TestInput(this.stringToLongList("109,-1,4,1,99"), -1),
+            TestInput(this.stringToLongList("109,-1,104,1,99"), 1),
+            TestInput(this.stringToLongList("109,-1,204,1,99"), 109),
+            TestInput(this.stringToLongList("109,1,9,2,204,-6,99"), 204),
+            TestInput(this.stringToLongList("109,1,109,9,204,-6,99"), 204),
+            TestInput(this.stringToLongList("109,1,209,-1,204,-106,99"), 204),
         )
         for (test in tests) {
             val (code, expectedOutput) = test
             val computer = IntcodeComputer(code)
+            assertEquals(expectedOutput, computer.process())
+        }
+    }
+
+    @Test
+    fun testProcessWithInput() {
+        val input = Random.nextLong()
+        val tests = listOf(
+            TestInput(this.stringToLongList("109,1,3,3,204,2,99"), input),
+            TestInput(this.stringToLongList("109,1,203,2,204,2,99"), input),
+        )
+        for (test in tests) {
+            val (code, expectedOutput) = test
+            val computer = IntcodeComputer(code)
+            computer.pushInput(input)
             assertEquals(expectedOutput, computer.process())
         }
     }
