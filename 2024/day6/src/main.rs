fe93d20afe5934581cd57a18bcee8cd157597b2c
PhilipEHausner <Philip.E.Hausner@gmail.com>
2024-12-15 13:02:30 +0100

    2024 Day 6 Part 2

--- 2024/day6/src/main.rs
+++ 2024/day6/src/main.rs
@@ -44,7 +44,7 @@ fn solve1(_input: &Input) -> usize {
     let mut input = _input.clone();
     loop {
         let npos = match next_pos(&input) {
-            WithinArea::FALSE => return input.visited.len(),
+            WithinArea::FALSE => return input.count_visited_fields(),
             WithinArea::TRUE(pos) => pos,
         };
         if is_obstacle(&input, &npos) {
@@ -94,7 +94,43 @@ fn is_obstacle(input: &Input, pos: &(usize, usize)) -> bool {
 }
 
 fn solve2(input: &Input) -> usize {
-    1
+    let mut result = 0;
+    for i in 0..input.size.0 {
+        for j in 0..input.size.1 {
+            if has_loop_with_additional_obstacle(input, (i, j)) {
+                result += 1;
+            }
+        }
+    }
+    result
+}
+
+fn has_loop_with_additional_obstacle(_input: &Input, obstacle: (usize, usize)) -> bool {
+    if _input.pos == obstacle {
+        return false;
+    }
+
+    let mut input = _input.clone();
+    input.field[obstacle.0][obstacle.1] = FieldType::OBSTACLE;
+
+    loop {
+        let npos = match next_pos(&input) {
+            WithinArea::FALSE => return false,
+            WithinArea::TRUE(pos) => pos,
+        };
+        if is_obstacle(&input, &npos) {
+            let ndirection = input.direction.turn_right();
+            if input.already_visited(&input.pos, &ndirection) {
+                return true;
+            }
+            input.direction = ndirection;
+        } else {
+            if input.already_visited(&npos, &input.direction) {
+                return true;
+            }
+            input.set_pos(npos);
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -103,7 +139,7 @@ enum FieldType {
     OBSTACLE,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
 enum Direction {
     UP,
     RIGHT,
@@ -135,13 +171,13 @@ struct Area {
     pos: (usize, usize),
     direction: Direction,
     size: (usize, usize),
-    visited: HashSet<(usize, usize)>,
+    visited: HashSet<(usize, usize, Direction)>,
 }
 
 impl Area {
     pub fn new(field: Field, pos: (usize, usize), direction: Direction) -> Area {
         let size = (field.len(), field[0].len());
-        let visited = HashSet::from([pos]);
+        let visited = HashSet::from([(pos.0, pos.1, direction)]);
         Area {
             field,
             pos,
@@ -152,8 +188,20 @@ impl Area {
     }
 
     fn set_pos(&mut self, pos: (usize, usize)) {
-        self.visited.insert(pos);
+        self.visited.insert((pos.0, pos.1, self.direction));
         self.pos = pos;
+    }
+
+    fn count_visited_fields(&self) -> usize {
+        self.visited
+            .iter()
+            .map(|v| (v.0, v.1))
+            .collect::<HashSet<_>>()
+            .len()
+    }
+
+    fn already_visited(&self, pos: &(usize, usize), direction: &Direction) -> bool {
+        self.visited.contains(&(pos.0, pos.1, direction.clone()))
     }
 }
 
@@ -175,5 +223,19 @@ mod tests {
         let input = get_input("./files/test.txt");
         let result = solve1(&input);
         assert_eq!(result, 41);
+    }
+
+    #[test]
+    fn test_solve2() {
+        let input = get_input("./files/day6.txt");
+        let result = solve2(&input);
+        assert_eq!(result, 1721);
+    }
+
+    #[test]
+    fn test_solve2_testdata() {
+        let input = get_input("./files/test.txt");
+        let result = solve2(&input);
+        assert_eq!(result, 6);
     }
 }
