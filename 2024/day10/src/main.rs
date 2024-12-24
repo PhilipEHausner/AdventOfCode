ec5b15695da794c6673df910027e1ac86720ded4
Philip Hausner <Philip.E.Hausner@gmail.com>
2024-12-24 14:07:28 +0100

    2024 Day 10 Part 2

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -3,7 +3,7 @@ use std::collections::{HashSet, VecDeque};
 use util::read_files::read_file_as_vector;
 
 fn main() {
-    let input = get_input("./files/test.txt");
+    let input = get_input("./files/day10.txt");
     println!("Solution part 1: {}", solve1(&input));
     println!("Solution part 2: {}", solve2(&input));
 }
@@ -73,7 +73,50 @@ fn dijkstra_adaptive(start: (usize, usize), input: &Input) -> usize {
 }
 
 fn solve2(input: &Input) -> usize {
-    1
+    let mut result = 0;
+
+    for (i, row) in input.iter().enumerate() {
+        for (j, &cell) in row.iter().enumerate() {
+            if cell == 0 {
+                result += dijkstra_adaptive_part2((i, j), input);
+            }
+        }
+    }
+
+    result
+}
+
+fn dijkstra_adaptive_part2(start: (usize, usize), input: &Input) -> usize {
+    let mut result = 0;
+    let (height, width) = (input.len(), input[0].len());
+    let mut visited = HashSet::new();
+    let mut queue = VecDeque::new();
+
+    queue.push_back(start);
+
+    while let Some((x, y)) = queue.pop_front() {
+        visited.insert((x, y));
+        let value = input[x][y];
+        if value == 9 {
+            result += 1;
+            continue;
+        }
+
+        for (dx, dy) in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
+            let nx = x as isize + dx;
+            let ny = y as isize + dy;
+
+            if nx >= 0 && ny >= 0 {
+                let (nx, ny) = (nx as usize, ny as usize);
+                if nx < height && ny < width && input[nx][ny] == value + 1 {
+                    visited.insert((nx, ny));
+                    queue.push_back((nx, ny));
+                }
+            }
+        }
+    }
+
+    result
 }
 
 type Input = Vec<Vec<i8>>;
@@ -86,7 +129,7 @@ mod tests {
     fn test_solve1() {
         let input = get_input("./files/day10.txt");
         let result = solve1(&input);
-        // assert_eq!(result, );
+        assert_eq!(result, 611);
     }
 
     #[test]
@@ -94,5 +137,18 @@ mod tests {
         let input = get_input("./files/test.txt");
         let result = solve1(&input);
         assert_eq!(result, 36);
+    }
+    #[test]
+    fn test_solve2() {
+        let input = get_input("./files/day10.txt");
+        let result = solve2(&input);
+        assert_eq!(result, 1380);
+    }
+
+    #[test]
+    fn test_solve2_testdata() {
+        let input = get_input("./files/test.txt");
+        let result = solve2(&input);
+        assert_eq!(result, 81);
     }
 }
