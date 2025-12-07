dbb080d4eac4c8f7049d058c627b70a539cb8dd2
Philip Hausner-Nguyen <Philip.E.Hausner@gmail.com>
2025-12-07 14:10:09 +0100

    Add preparations 2025

---
+++ 2025/template/main.rs
@@ -0,0 +1,41 @@
+use util::read_files::read_file_as_vector;
+
+fn main() {
+    let input = get_input("./files/test.txt");
+    println!("Solution part 1: {}", solve1(&input));
+    println!("Solution part 2: {}", solve2(&input));
+}
+
+fn get_input(filename: &str) -> Input {
+    let lines = read_file_as_vector(filename).expect("Cannot read file.");
+    1
+}
+
+fn solve1(input: &Input) -> usize {
+    1
+}
+
+fn solve2(input: &Input) -> usize {
+    1
+}
+
+type Input = i64;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve1() {
+        let input = get_input("./files/day.txt");
+        let result = solve1(&input);
+        // assert_eq!(result, );
+    }
+
+    #[test]
+    fn test_solve1_testdata() {
+        let input = get_input("./files/test.txt");
+        let result = solve1(&input);
+        // assert_eq!(result, );
+    }
+}
