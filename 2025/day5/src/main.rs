d7d9b211235abef71cfc6fcc35788f81e22bcd26
Philip Hausner-Nguyen <Philip.E.Hausner@gmail.com>
2025-12-07 21:40:32 +0100

    2025 - Day 5 Part 2

--- 2025/day5/src/main.rs
+++ 2025/day5/src/main.rs
@@ -48,7 +48,7 @@ fn solve1(input: &Input) -> usize {
 }
 
 fn solve2(input: &Input) -> usize {
-    1
+    get_num_fresh_ids(&input.intervals)
 }
 
 fn is_fresh(intervals: &Vec<Interval>, candidate: u64) -> bool {
@@ -61,6 +61,55 @@ fn is_fresh(intervals: &Vec<Interval>, candidate: u64) -> bool {
     false
 }
 
+fn get_num_fresh_ids(intervals: &Vec<Interval>) -> usize {
+    let mut intervals = intervals.to_vec();
+
+    loop {
+        let mut fusion = false;
+        let mut new_intervals = vec![];
+        let mut fusioned_intervals = vec![];
+
+        for i in 0..intervals.len() {
+            for j in (i + 1)..intervals.len() {
+                if fusioned_intervals.contains(&j) {
+                    continue;
+                }
+                if overlaps(&intervals[i], &intervals[j]) {
+                    fusion = true;
+                    fusioned_intervals.push(i);
+                    fusioned_intervals.push(j);
+                    let new_interval = Interval {
+                        from: u64::min(intervals[i].from, intervals[j].from),
+                        to: u64::max(intervals[i].to, intervals[j].to),
+                    };
+                    new_intervals.push(new_interval);
+                }
+            }
+
+            if !fusioned_intervals.contains(&i) {
+                new_intervals.push(intervals[i]);
+            }
+        }
+
+        intervals = new_intervals;
+
+        if !fusion {
+            break;
+        }
+    }
+
+    intervals
+        .iter()
+        .map(|interval| (interval.to - interval.from + 1) as usize)
+        .sum()
+}
+
+fn overlaps(interval1: &Interval, interval2: &Interval) -> bool {
+    (interval1.from <= interval2.to && interval1.to >= interval2.from)
+        || (interval2.from <= interval1.to && interval2.to >= interval1.from)
+}
+
+#[derive(Clone, Copy, Debug)]
 struct Interval {
     from: u64,
     to: u64,
@@ -95,13 +144,13 @@ mod tests {
     fn test_solve2() {
         let input = get_input("./files/day5.txt");
         let result = solve2(&input);
-        // assert_eq!(result, );
+        assert_eq!(result, 358155203664116);
     }
 
     #[test]
     fn test_solve2_testdata() {
         let input = get_input("./files/test.txt");
         let result = solve2(&input);
-        // assert_eq!(result, );
+        assert_eq!(result, 14);
     }
 }
