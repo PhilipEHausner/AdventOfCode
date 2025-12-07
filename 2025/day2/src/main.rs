6fa49397f131acabe9fdcd7f7a5cebba8e947d96
Philip Hausner-Nguyen <Philip.E.Hausner@gmail.com>
2025-12-07 15:19:48 +0100

    2025 - Day 2 Part 1

---
+++ 2025/day2/src/main.rs
@@ -0,0 +1,90 @@
+use util::read_files::read_file_as_vector;
+
+fn main() {
+    let input = get_input("./files/day2.txt");
+    println!("Solution part 1: {}", solve1(&input));
+    println!("Solution part 2: {}", solve2(&input));
+}
+
+fn get_input(filename: &str) -> Input {
+    let lines = read_file_as_vector(filename).expect("Cannot read file.");
+    let line = lines.first().unwrap();
+    line.split(',')
+        .map(|interval| {
+            let parts: Vec<&str> = interval.split('-').collect();
+            Interval {
+                from: parts.get(0).unwrap().parse().unwrap(),
+                to: parts.get(1).unwrap().parse().unwrap(),
+            }
+        })
+        .collect()
+}
+
+fn solve1(input: &Input) -> usize {
+    let mut result = 0;
+    for interval in input {
+        for number in interval.from..=interval.to {
+            if is_illegal_number(number) {
+                result += number as usize;
+            }
+        }
+    }
+    result
+}
+
+fn solve2(input: &Input) -> usize {
+    1
+}
+
+fn is_illegal_number(number: u64) -> bool {
+    let str = number.to_string();
+    if str.len() % 2 != 0 {
+        return false;
+    }
+
+    if str[..str.len() / 2] == str[str.len() / 2..] {
+        return true;
+    }
+
+    false
+}
+
+struct Interval {
+    from: u64,
+    to: u64,
+}
+
+type Input = Vec<Interval>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve1() {
+        let input = get_input("./files/day.txt");
+        let result = solve1(&input);
+        // assert_eq!(result, );
+    }
+
+    #[test]
+    fn test_solve1_testdata() {
+        let input = get_input("./files/test.txt");
+        let result = solve1(&input);
+        // assert_eq!(result, );
+    }
+
+    #[test]
+    fn test_solve2() {
+        let input = get_input("./files/day.txt");
+        let result = solve2(&input);
+        // assert_eq!(result, );
+    }
+
+    #[test]
+    fn test_solve2_testdata() {
+        let input = get_input("./files/test.txt");
+        let result = solve2(&input);
+        // assert_eq!(result, );
+    }
+}
