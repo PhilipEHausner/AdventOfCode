1949288371d1a9d0525b7069b62c5e3586df5007
Philip Hausner-Nguyen <Philip.E.Hausner@gmail.com>
2025-12-07 15:34:45 +0100

    2025 - Day 2 Part 2

--- 2025/day2/src/main.rs
+++ 2025/day2/src/main.rs
@@ -33,7 +33,15 @@ fn solve1(input: &Input) -> usize {
 }
 
 fn solve2(input: &Input) -> usize {
-    1
+    let mut result = 0;
+    for interval in input {
+        for number in interval.from..=interval.to {
+            if is_illegal_number_part2(number) {
+                result += number as usize;
+            }
+        }
+    }
+    result
 }
 
 fn is_illegal_number(number: u64) -> bool {
@@ -44,6 +52,28 @@ fn is_illegal_number(number: u64) -> bool {
 
     if str[..str.len() / 2] == str[str.len() / 2..] {
         return true;
+    }
+
+    false
+}
+
+fn is_illegal_number_part2(number: u64) -> bool {
+    let str_num = number.to_string();
+
+    for l in 1..=(str_num.len() / 2) {
+        if str_num.len() % l != 0 {
+            continue;
+        }
+
+        let mut parts: Vec<&str> = vec![];
+        for x in 0..(str_num.len() / l) {
+            parts.push(&str_num[x * l..x * l + l]);
+        }
+
+        let first_part = parts.first().unwrap();
+        if parts.iter().all(|part| part == first_part) {
+            return true;
+        }
     }
 
     false
@@ -62,29 +92,29 @@ mod tests {
 
     #[test]
     fn test_solve1() {
-        let input = get_input("./files/day.txt");
+        let input = get_input("./files/day2.txt");
         let result = solve1(&input);
-        // assert_eq!(result, );
+        assert_eq!(result, 54641809925);
     }
 
     #[test]
     fn test_solve1_testdata() {
         let input = get_input("./files/test.txt");
         let result = solve1(&input);
-        // assert_eq!(result, );
+        assert_eq!(result, 1227775554);
     }
 
     #[test]
     fn test_solve2() {
-        let input = get_input("./files/day.txt");
+        let input = get_input("./files/day2.txt");
         let result = solve2(&input);
-        // assert_eq!(result, );
+        assert_eq!(result, 73694270688);
     }
 
     #[test]
     fn test_solve2_testdata() {
         let input = get_input("./files/test.txt");
         let result = solve2(&input);
-        // assert_eq!(result, );
+        assert_eq!(result, 4174379265);
     }
 }
